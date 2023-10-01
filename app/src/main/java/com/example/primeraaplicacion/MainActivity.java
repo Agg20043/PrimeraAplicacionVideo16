51ca053142b17aa8d1cc7acc70eed5d91e784137
Agg20043 <alvarogarciaguisado@educarex.es>
2023-10-01 22:15:19 +0200

    primer programa

--- app/src/main/java/com/example/primeraaplicacion/MainActivity.java
+++ app/src/main/java/com/example/primeraaplicacion/MainActivity.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 public class MainActivity extends Activity {
     public int contador;
 
@@ -33,7 +31,8 @@ public class MainActivity extends Activity {
         mostrarResultado();
     }
     public void mostrarResultado(){
-        TextView textoResultado = (TextView)findViewById(R.id.ContadorPulsaciones);
-        textoResultado.setText("Contador: " + contador);
+        TextView textoResultado = (TextView)findViewById(R.id.contadorTexto);
+        textoResultado.setText("" + contador);
+
     }
 }
